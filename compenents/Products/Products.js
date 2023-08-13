a7555b56554da01a86626a11938858aa1093d9f0
thebrokenblow <artkrasov@gmail.com>
2023-08-14 01:09:39 +0300

    Added a counter of goods

--- compenents/Products/Products.js
+++ compenents/Products/Products.js
@@ -14,6 +14,7 @@ class Products {
       element.classList.remove(this.classNameActive);
       element.innerHTML = this.labelAdd;
     }
+    headerPage.render(products.length);
   }
 
   render() {
