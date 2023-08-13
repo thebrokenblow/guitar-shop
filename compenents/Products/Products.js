34f8e1f7c0e0d98cccff6746a39afd45fa2507a6
thebrokenblow <artkrasov@gmail.com>
2023-08-14 00:41:56 +0300

    Added adding product in localstorage on click button

--- compenents/Products/Products.js
+++ compenents/Products/Products.js
@@ -1,8 +1,37 @@
 class Products {
+  constructor() {
+    this.classNameActive = "products-element__btn_active";
+    this.labelAdd = "Добавить в корзину";
+    this.labelRemove = "Удалить из корзины";
+  }
+
+  handleSetLocationStorage(element, id) {
+    const { isPushProduct, products } = localStorageUtil.putProducts(id);
+    console.log(isPushProduct);
+    if (isPushProduct) {
+      element.classList.add(this.classNameActive);
+      element.innerHTML = this.labelRemove;
+    } else {
+      element.classList.remove(this.classNameActive);
+      element.innerHTML = this.labelAdd;
+    }
+  }
+
   render() {
+    const productsStore = localStorageUtil.getProducts();
     let htmlCatalog = "";
 
     CATALOG.forEach(({ id, name, price, img }) => {
+      let activeClass = "";
+      let activeText = "";
+
+      if (productsStore.indexOf(id) === -1) {
+        activeText = this.labelAdd;
+      } else {
+        activeClass = " " + this.classNameActive;
+        activeText = this.labelRemove;
+      }
+
       htmlCatalog += `
                 <li class="products-element">
                     <span class="products-element__name">${name}</span>
@@ -10,7 +39,9 @@ class Products {
                     <span class="products-element__price">
                         ⚡️ ${price.toLocaleString()} USD
                     </span>
-                    <button class="products-element__btn">Добавить в корзину</button>
+                    <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
+                        ${activeText}
+                    </button>
                 </li>
             `;
     });
