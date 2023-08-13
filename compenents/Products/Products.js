2ce6011589e8785f683943474d06821c96fed1ab
thebrokenblow <artkrasov@gmail.com>
2023-08-13 07:33:16 +0300

    Added styles for products list

--- compenents/Products/Products.js
+++ compenents/Products/Products.js
@@ -1,24 +1,29 @@
 class Products {
   render() {
     let htmlCatalog = "";
+
     CATALOG.forEach(({ id, name, price, img }) => {
       htmlCatalog += `
-            <li>
-                <span> ${name} </span>
-                <img src="${img}"/>
-                <span> ${price} </span>
-                <button>Добавить в корзину</button>
-            </li>
-        `;
+                <li class="products-element">
+                    <span class="products-element__name">${name}</span>
+                    <img class="products-element__img" src="${img}" />
+                    <span class="products-element__price">
+                        ⚡️ ${price.toLocaleString()} USD
+                    </span>
+                    <button class="products-element__btn">Добавить в корзину</button>
+                </li>
+            `;
     });
+
     const html = `
-        <ul>
-            ${htmlCatalog}
-        </ul>
-    `;
+            <ul class="products-container">
+                ${htmlCatalog}
+            </ul>
+        `;
+
     ROOT_PRODUCTS.innerHTML = html;
   }
 }
 
-const products = new Products();
-products.render();
+const productsPage = new Products();
+productsPage.render();
